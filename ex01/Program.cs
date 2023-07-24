d796170197b766e41d32fbc5bb15af44d6abd554
ausziehen <marimaki@bk.ru>
2023-07-24 18:58:27 +0300

    1

---
+++ ex01/Program.cs
@@ -0,0 +1,11 @@
+﻿int Degree(int numb1, int numb2)
+{
+    int result = 1;
+    for (int i = 0; i < numb2; i++) result *= numb1;
+    return result;
+}
+Console.Write("Input a first number: ");
+int num1 = Convert.ToInt32(Console.ReadLine());
+Console.Write("Input a second number: ");
+int num2 = Convert.ToInt32(Console.ReadLine());
+Console.Write("Your number is: " + Degree(num1, num2));
